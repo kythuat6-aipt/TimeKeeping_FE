2c7e59c53adf799c72f96b5cce1b1a2762ca287a
kythuat6-aipt <kythuat6@aipt.vn>
2024-08-01 17:28:56 +0700

    upload file build

---
+++ build.sh
@@ -0,0 +1,20 @@
+# get new code
+npm i
+git checkout main
+git pull
+
+# build
+NODE_OPTIONS="--max_old_space_size=4096" npm run build
+mv build/static/* build
+rm build/static -rf
+rm build/robots.txt
+
+# handle templates
+rm ../Back_End/src/templates -rf
+mkdir ../Back_End/src/templates
+mv build/index.html ../Back_End/src/templates/index.html
+
+# handle web
+rm ../Back_End/src/static -rf
+mkdir ../Back_End/src/static
+mv build ../Back_End/src/static/web
