89b96b16bb8e96dc097125ada5be6c6b4ec766ed
kythuat6-aipt <kythuat6@aipt.vn>
2024-12-29 23:35:57 +0700

    .

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@
 # git pull
 npm i
 
-# build
-NODE_OPTIONS="--max_old_space_size=4096" npm run build
-mv build/static/* build
-rm build/static -rf
-rm build/robots.txt
+# # build
+# NODE_OPTIONS="--max_old_space_size=4096" npm run build
+# mv build/static/* build
+# rm build/static -rf
+# rm build/robots.txt
 
-# handle templates
-rm ../Back_End/src/templates -rf
-mkdir ../Back_End/src/templates
-mv build/index.html ../Back_End/src/templates/index.html
+# # handle templates
+# rm ../Back_End/src/templates -rf
+# mkdir ../Back_End/src/templates
+# mv build/index.html ../Back_End/src/templates/index.html
 
 # handle web
-rm ../Back_End/src/static -rf
-mkdir ../Back_End/src/static
-mv build ../Back_End/src/static/web
+# rm ../Back_End/src/static -rf
+# mkdir ../Back_End/src/static
+# mv build ../Back_End/src/static/web
